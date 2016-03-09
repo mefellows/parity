264296ff38eed72c2b538912a909c07b20fb5d17
Matt Fellows <matt.fellows@onegeek.com.au>
2016-03-09 22:44:59 +1100

    Latest SHASUMS

--- scripts/parity.rb
+++ scripts/parity.rb
@@ -6,10 +6,10 @@ class Parity < Formula
 
   if Hardware.is_64_bit?
     url "https://github.com/mefellows/parity/releases/download/v#{version}/darwin_amd64.zip"
-    sha1 ''
+    sha1 '411503eaef44cd14896564495c94c332fda82892'
   else
     url "https://github.com/mefellows/parity/releases/download/v#{version}/darwin_386.zip"
-    sha1 ''
+    sha1 'd1a8c956b629ccbaee9b24e44c35747df747f8a8'
   end
 
   depends_on :arch => :intel
