ef43c9718a051b2fcf4c023026137ac292ace6a0
Matt Fellows <matt.fellows@onegeek.com.au>
2016-03-18 13:47:16 +1100

    Updated release SHAs

--- scripts/parity.rb
+++ scripts/parity.rb
@@ -6,10 +6,10 @@ class Parity < Formula
 
   if Hardware.is_64_bit?
     url "https://github.com/mefellows/parity/releases/download/#{version}/darwin_amd64.zip"
-    sha1 ''
+    sha1 'ab7c3e649d8ad45f504e2bc5c9d25190a018e383'
   else
     url "https://github.com/mefellows/parity/releases/download/#{version}/darwin_386.zip"
-    sha1 ''
+    sha1 '57ff641c30bc2f2bec900abf8f5ab3f804ab8202'
   end
 
   depends_on :arch => :intel
